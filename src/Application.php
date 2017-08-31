2a5459bba7e0d9e6f50d6bde1d2c97bc049087e9
Kasutaja <varjekass@gmail.com>
2017-08-31 14:12:41 +0300

    show list of bookings on list.html page

--- src/Application.php
+++ src/Application.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace BookingApp;
-
 use BookingApp\Controllers\CreateBookingController;
+use BookingApp\Controllers\ListBookingsController;
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\LocaleServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
-
 /**
  * Custom Application class that hold our application specifix functionality.
  */
@@ -18,23 +16,19 @@ class Application extends SilexApplication
     public function __construct(array $values = [])
     {
         parent::__construct($values);
-
         $this->configureServices();
         $this->createDBTables();
         $this->configureControllers();
     }
-
     /**
      * Config app options and register services.
      */
     private function configureServices()
     {
         $this['debug'] = true;
-
         $this->register(new TwigServiceProvider(), [
             'twig.path' => __DIR__.'/../views',
         ]);
-
         // Database configuration
         $this->register(new DoctrineServiceProvider(), [
             'db.options' => [
@@ -42,14 +36,12 @@ class Application extends SilexApplication
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
-
         $this->register(new FormServiceProvider());
         $this->register(new LocaleServiceProvider());
         $this->register(new TranslationServiceProvider(), [
             'translator.domains' => [],
         ]);
     }
-
     /**
      * Creates all needed tables to database if they don't exist.
      */
@@ -72,7 +64,6 @@ class Application extends SilexApplication
             );");
         }
     }
-
     /**
      * Define all used routes and connect a route to its controller.
      */
@@ -81,8 +72,13 @@ class Application extends SilexApplication
         $this
             ->match('/bookings/create', new CreateBookingController(
                 $this['form.factory'],
-                $this['twig']
+                $this['twig'],
+                $this['db']
             ))
+            ->method('GET|POST')
+        ;
+        $this
+            ->get('/bookings', new ListBookingsController($this['db'], $this['twig']))
         ;
     }
 }
---
+++ src/Controllers/ListBookingsController.php
@@ -0,0 +1,19 @@
+<?php
+namespace BookingApp\Controllers;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\Request;
+class ListBookingsController
+{
+    public function __construct(Connection $dbConnection, \Twig_Environment $twigEnv)
+    {
+        $this->dbConnection = $dbConnection;
+        $this->twigEnv = $twigEnv;
+    }
+    public function __invoke(Request $request)
+    {
+        $query = $this->dbConnection->executeQuery("SELECT * FROM bookings");
+        alert($query);
+        $result = $query->fetchAll();
+        return $this->twigEnv->render('list.html.twig', ['bookings' => $result]);
+    }
+}
